5e6aa26e4e99249971d4e2190bc3cb438ccdcd74
Tim McLaughlin <Tim.McLaughlin@wwu.edu>
2022-12-01 15:46:13 -0800

    Day 1 is working, now Tracking elves with objects to allow for more data in the future puzzles

---
+++ day1/part1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+class Elf:
+  def __init__(self,id):
+    self.id = id
+    self.calories = 0
+
+input_file = open('day1/input1.txt','r')
+input_lines = input_file.readlines()
+
+elves = []
+cur_elf = Elf(0)
+max_cal_elf = cur_elf
+
+for line in input_lines:
+  # start adding numbers, storing when we hit a blank line
+  if line in ['\n','\r\n']:
+    # we're done with this elf's pack
+
+    # is this the biggest? update things if so
+    if cur_elf.calories > max_cal_elf.calories:
+      max_cal_elf = cur_elf
+
+    # put the previous elf in the list and start on a new one
+    elves.append(cur_elf)
+    cur_elf = Elf(len(elves))
+  else:
+    cur_elf.calories += int(line)
+
+print("Elf {} has the most calories, with {}\n".format(max_cal_elf.id,max_cal_elf.calories))
